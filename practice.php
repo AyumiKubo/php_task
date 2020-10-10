aee727743f91d8c0c09b12a3c418b3dd17e2edcb
EC2 Default User <stretch_plueme@hotmail.com>
2020-10-10 03:21:12 +0000

    php_task

---
+++ practice.php
@@ -0,0 +1,33 @@
+<?php
+echo 'hello php!';
+
+$a = 3;
+$b = 7;
+echo $a + $b;
+$array_month = ['1月', '2月', '3月', '４月', '５月', '６月', '７月', '８月', '９月', '10月', '11月', '12月'];
+echo $array_month[7];
+
+$hello = "Hello,";
+$name = "Ayumi";
+$world = "'s World!";
+echo $hello .= $name .= $world;
+
+$tech_boost = 'tech ';
+$tech_boost .= 'boost';
+echo $tech_boost;
+
+$calender_2018 = [
+    "January" => "1月",
+    "February" => "2月",
+    "March" => "3月",
+    "April" => "4月",
+    "May" => "5月",
+    "June" => "6月",
+    "August" => "8月",
+    "September" => "9月",
+    "October" => "10月",
+    "November" => "11月",
+    "December" => "12月"
+];
+    echo $calender_2018["December"];
+    
